c470e2165d6c5b1c4094075e70e1479142ce2525
dimas wicaksono <novan.top@gmail.com>
2019-09-02 16:11:49 +0700

    :fire:

---
+++ countOddNumbersbelow.js
@@ -0,0 +1,11 @@
+function oddCount(n) {
+  // your code here
+  //   if (n % 2 !== 0) {
+  //     return Math.floor(n / 2);
+  //   }
+
+  if (n % 2 !== 0) return Math.floor(n / 2);
+}
+
+console.log(oddCount(15), 7, "Oops! Wrong.");
+console.log(oddCount(15023), 7511, "Oops! Wrong.");

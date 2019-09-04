81bd460647fbde827392bfc265f5680d079422dc
dimas wicaksono <novan.top@gmail.com>
2019-09-05 00:28:32 +0700

    :fire:

---
+++ guardingAlgolandia.js
@@ -0,0 +1,14 @@
+function findNeededGuards(k) {
+  // your beatiful code goes here ;)
+  var countTrue = 0;
+  var countfalse = 0;
+  var count = 0;
+  for (let i = 0; i < k.length - 1; i++) {
+    if (k[i] !== true && k[i + 1] !== true) count++;
+  }
+
+  return count;
+}
+
+console.log(findNeededGuards([true, true, false, true, false])); // 0
+console.log(findNeededGuards([false, false, true, false, false])); // 2

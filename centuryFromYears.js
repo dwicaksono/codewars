2bd441f49611f190f1427861e58b76f243f202e7
dimas wicaksono <novan.top@gmail.com>
2019-09-09 09:59:05 +0700

    :fire:

--- centuryFromYears.js
+++ centuryFromYears.js
@@ -1,11 +1,14 @@
 function century(year) {
   // Finish this :)
 
-  var years = Math.round(year / 100 + 1);
-  if (year <= 100) return 1;
-  //   if (year > 100 && year % 100 === 0) return Math.round(year / 100);
-  if (year > 100 && year % 100 !== 0) return Math.round(year / 100 + 1);
-  if (year % 1000 !== 0) return Math.round(year / 100);
+  // var years = parseInt(year / 100);
+  // if (year <= 100) return 1;
+  // if (year % 2 === 0) {
+  //   return years;
+  // } else {
+  //   return years + 1;
+  // }
+  return Math.ceil(year / 100);
 }
 
 console.log(century(1705), 18, "Testing for year 1705");
@@ -13,3 +16,4 @@ console.log(century(1900), 19, "Testing for year 1900");
 console.log(century(1601), 17, "Testing for year 1601");
 console.log(century(89), 1, "Testing for year 89");
 console.log(century(530559), 5306, "Testing for year 5306");
+console.log(century(388767), 3888, "Testing for year 3888");

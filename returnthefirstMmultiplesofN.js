cb364a8d7b0d95eb491fd8c2161f3eb3dfce2b92
dimas wicaksono <novan.top@gmail.com>
2019-09-09 22:14:40 +0700

    :fire:

---
+++ returnthefirstMmultiplesofN.js
@@ -0,0 +1,10 @@
+function multiples(m, n) {
+  // code here
+
+  var result = [];
+  console.log(i);
+  for (let i = 1; i <= m; i++) result.push(n * i);
+  return result;
+}
+
+console.log(multiples(3, 5)); // [5, 10, 15]

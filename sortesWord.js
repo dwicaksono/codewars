2a6655e2f5f25259437f44fc49d27ca916d4cedf
dimas wicaksono <novan.top@gmail.com>
2019-09-10 10:01:31 +0700

    :fire:

---
+++ sortesWord.js
@@ -0,0 +1,34 @@
+function findShort(s) {
+  // var arr = s.split(' ');
+  ///split without split
+  var sArr = [];
+  var temp = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") {
+      sArr.push(temp);
+      temp = "";
+    } else {
+      temp += s[i];
+    }
+  }
+  sArr.push(temp);
+  ////////////smallest number without sort
+  var smallest = sArr[0];
+  for (var i = 0; i < sArr.length; i++) {
+    if (sArr[i].length < smallest.length) {
+      smallest = sArr[i];
+    }
+  }
+  return smallest.length;
+}
+
+console.log(
+  findShort("bitcoin take over the world maybe who knows perhaps"),
+  3
+);
+console.log(
+  findShort(
+    "turns out random test cases are easier than writing out basic ones"
+  ),
+  3
+);

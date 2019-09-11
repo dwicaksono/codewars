943cbf709a2f3f3744d0adb9997d25c7a13260f8
dimas wicaksono <novan.top@gmail.com>
2019-09-11 23:54:49 +0700

    :fire:

---
+++ numberWaitLetter.js
@@ -0,0 +1,18 @@
+function doMath(s) {
+  //your code here
+  s;
+  //  var num = parseFloat(s)
+  //   var num = 0;
+  for (let i = 0; i < s.length; i++) {
+    console.log(s[i]);
+    var num = parseFloat(s);
+    num;
+  }
+  num;
+}
+
+console.log(doMath("24z6 1z23 y369 89z 900b"), 1414);
+// console.log(doMath("24z6 1x23 y369 89a 900b"), 1299);
+// console.log(doMath("10a 90x 14b 78u 45a 7b 34y"), 60);
+// console.log(doMath("111a 222c 444y 777u 999a 888p"), 1459);
+// console.log(doMath("1z 2t 3q 5x 6u 8a 7b"), 8);
